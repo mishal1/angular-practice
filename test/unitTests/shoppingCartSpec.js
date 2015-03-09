6b8d32b86f8cbf2afa9d34fa8118105a239a597c
Mishal Islam <mishal.islam01@gmail.com>
2015-03-09 12:22:18 +0000

    can see total price of items in a cart

--- test/unitTests/shoppingCartSpec.js
+++ test/unitTests/shoppingCartSpec.js
@@ -7,10 +7,15 @@ describe('Shopping Cart', function(){
   beforeEach(function(){
     cart = new ShoppingCart()
     shoe = jasmine.createSpy('shoe');
+    shoe.price = 2
   });
 
   it('is empty when initialized', function(){
     expect(cart.items).toEqual([])
+  });
+
+  it('has a total price of 0 when initialized', function(){
+    expect(cart.totalPrice).toEqual(0)
   });
 
   describe('after an item is added to the shooping cart', function(){
@@ -26,6 +31,15 @@ describe('Shopping Cart', function(){
     it('it can be removed', function(){
       cart.remove(shoe)
       expect(cart.items).toEqual([])
+    });
+
+    it('the total price changes', function(){
+      expect(cart.totalPrice).toEqual(2)
+    });
+
+    it('the total price decreases after an item is removed from the cart', function(){
+      cart.remove(shoe)
+      expect(cart.totalPrice).toEqual(0)
     });
 
   });
