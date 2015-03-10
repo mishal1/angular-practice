7b31384cb4bd462417f78b7dc17866eb0784f212
Mishal Islam <mishal.islam01@gmail.com>
2015-03-10 17:41:39 +0000

    displaying casual wear elements

--- public/js/app.js
+++ public/js/app.js
@@ -9,4 +9,21 @@ app.controller('mainCtrl', function($scope, $http){
     var menu = document.getElementById('women-menu')
     menu.style.display = 'none'
   }
+  $scope.showCasualwear = function(){
+    $http.get('mockDatabase/products.json')
+      .success(function(products){
+        $scope.list = []
+        console.log()
+        for(var key in products){
+          if(products[key].category === "Women's Casualwear"){
+            $scope.list.push(products[key])
+            console.log($scope.list)
+          }
+        }
+      })
+      .error(function(error, status, headers, config){
+        console.log('error')
+        console.log(error)
+      })
+  }
 });
