1ac5160b2e57882f2b0b37e80bbf2f6e1fd98347
Mishal Islam <mishal.islam01@gmail.com>
2015-03-10 10:33:12 +0000

    added top bar

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(request, response){
   response.render('index.ejs')
 });
